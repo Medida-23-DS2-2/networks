cd9a6bec59cea24901ee44e7f111e87101681a06
nosc364d <nora.schaefer@mailbox.tu-dresden.de>
2023-06-19 16:59:45 +0200

    added title screen

--- interface/Pause.gd
+++ interface/Pause.gd
@@ -4,32 +4,22 @@ extends Control
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-onready var pause = $HSplitContainer/Button
-onready var brightness = get_node("/root/Node2D/DarkOverlay")
+onready var pause = $HBoxContainer/Button
+onready var root = get_node("/root/Node2D")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	brightness.hide()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 
 func _on_pause_button_pressed():
-	if (get_tree().paused):
-		# if game is already paused:
-		# remove all pause menus, options etc
-		pause.text = "Pause"
-		# set brightness to 100%
-		brightness.hide()
-		# resume game
-		get_tree().paused = false
-	else:
-		# brightness = 50%
-		brightness.show()
+	if (!get_tree().paused):
 		# show menu
-		pause.text = "Weiter"
-		# pause all other actions
-		get_tree().paused = true
+		var pause_menu = load("res://interface/PauseMenu.tscn")
+		var pause_instance = pause_menu.instance()
+		root.add_child(pause_instance)
 
 
---
+++ interface/PauseMenu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_tree().paused = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_ResumeButton_pressed():
+	get_tree().paused = false
+	queue_free()
